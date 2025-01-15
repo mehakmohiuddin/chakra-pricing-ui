403ebeda5ad5e1411d4f7c98a39820660da455f3
Mehak Mohe ud Din <mehak.mohiuddin254@gmail.com>
2025-01-15 23:07:10 +0500

    3rd commit

--- pages/api/_app.tsx
+++ pages/api/_app.tsx
@@ -1,8 +1,10 @@
 import { ChakraProvider } from "@chakra-ui/react";
-export default function App({Component , pageProps}:any){
-return(
+import { AppProps } from "next/app";
+
+export default function App({ Component, pageProps }: AppProps) {
+  return (
     <ChakraProvider>
-        <Component {...pageProps}/>
+      <Component {...pageProps} />
     </ChakraProvider>
-)
+  );
 }
